84ace5e1bce564f187ae275a0b408d85180998e1
jx0424 <jl1280@live.mdx.ac.uk>
2024-04-06 23:33:18 +0100

    Used other approach
    trying back the template method and its working wonders. The program automatically click on skip intro, skip recap and next episode. Noticed that when next episode button comes out it stays on the edge of the button and doesnt click, therefore added +5 to x and y axis of the coordinate. All works perfectly now

--- netflixAutoSkipper.py
+++ netflixAutoSkipper.py
@@ -1,57 +1,57 @@
+import time
+import pyautogui
 import numpy as np
 import cv2
-import pyautogui 
-import time
-import pytesseract
 
-# Set the path to tesseract executable
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-
+# Continuos loop for see if there are any Skip Recap/ Skip Intro or Next Episode button
 while True:
+    time.sleep(1)
+# Get a screenshot of the screen
     img = pyautogui.screenshot()
-    frame = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2GRAY)
+    img = np.asarray(img.convert(mode = 'L'))
+    path_intro_template = r'C:\Users\brend\OneDrive - Middlesex University\Documents\GitHub\Netflix-IntroOST-Auto_Skipper\skipIntro1.png'
+    path_recap_template = r'C:\Users\brend\OneDrive - Middlesex University\Documents\GitHub\Netflix-IntroOST-Auto_Skipper\skipRecap.png'
+    path_next_episode_template = r'C:\Users\brend\OneDrive - Middlesex University\Documents\GitHub\Netflix-IntroOST-Auto_Skipper\nextEpisode.png'
+    intro_template = cv2.imread(path_intro_template, 0)
+    recap_template = cv2.imread(path_recap_template, 0)
+    episode_template = cv2.imread(path_next_episode_template, 0)
 
-    # Apply OCR to the screenshot
-    text = pytesseract.image_to_string(frame)
+# Set a threshold to get a better result
+    threshold = 0.8
 
-    # Check if "Skip" text is present
-    if "Skip Intro" in text:
-        print("Skip button found!")
-        # Add your code here to handle the skip button
+# Check for skip intro  
+    res = cv2.matchTemplate(img, intro_template, cv2.TM_CCOEFF_NORMED)
+    loc = np.where(res >= threshold)
 
-# import numpy as np
-# import cv2
-# import pyautogui 
-# import time
-# import pytesseract
+# Check if intro template is matched
+    if loc[0].size != 0:
+        x = loc[1]
+        y = loc[0]
+# Click on the location of the button
+        pyautogui.click(x, y)
+        continue # continue loop from start without further execution of the loop
 
-# #NEEDS to have an realtime update from Comptuer screen! use pyautogui.screenshot()
-# template_path = r'C:\Users\brend\OneDrive - Middlesex University\Documents\GitHub\Netflix-IntroOST-Auto_Skipper\skipIntroButton.png'
+# Check for skip recap
+    res = cv2.matchTemplate(img, recap_template, cv2.TM_CCOEFF_NORMED)
+    loc = np.where(res >= threshold)
 
-# while True:
-#     img = pyautogui.screenshot()
-#     frame = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2GRAY)
-#     template = cv2.imread(template_path, 0)
-#     height, width = template.shape
+# Check if recap template is matched
+    if loc[0].size != 0:
+        x = loc[1]
+        y = loc[0]
+# Click on the location of the button
+        pyautogui.click(x, y)
+        continue # continue loop from start without further execution of the loop
 
-#     frame = cv2.GaussianBlur(frame, (5, 5), 0)
-#     template = cv2.GaussianBlur(template, (5, 5), 0)
-#     img2 = frame.copy()
-#     result = cv2.matchTemplate(img2, template, cv2.TM_CCOEFF_NORMED)
-#     min_val ,max_val, min_loc, max_loc = cv2.minMaxLoc(result)
-#     top_left = min_loc
-#     bottom_right = (top_left[0] + width, top_left[1] + height)
-#     print('top left' , top_left)
-#     print('bot_right' , bottom_right)
-#     cv2.rectangle(img2, top_left, bottom_right, 0, 5)
-#     cv2.imshow("Results", img2)
-#     cv2.waitKey(0)
-#     cv2.destroyAllWindows()
+# Check for next episode
+    res = cv2.matchTemplate(img, episode_template, cv2.TM_CCOEFF_NORMED)
+    loc = np.where(res >= threshold)
 
-#     if min_loc:
-#         time.sleep(2)
-#         x = (top_left[0] + bottom_right[0])/2
-#         y = (top_left[1] + bottom_right[1])/2
-#         print(x,y)
-#         pyautogui.moveTo(x , y, 2)
-#         pyautogui.click()
+# Check if episode template is matched
+    if loc[0].size != 0:
+        x = loc[1]
+        y = loc[0]       
+# Click on the location of the button
+        pyautogui.click(x + 5, y + 5)
+        continue # continue loop from start without further execution of the loop
+
